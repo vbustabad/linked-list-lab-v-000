82f51c6f98f6ca99d74d9070f2319e3e3afe9df5
Veronica Bustabad <vbustabad@gmail.com>
2019-01-23 15:01:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,21 +13,41 @@ function headNode(linkedList, collection) {
 }
 
 function nodeAt(index, linkedList, collection) {
+<<<<<<< HEAD
   let node = headNode(linkedList, collection);
   //let head = node;
 
   for (let i = 0; i < index; i++) {
      node = next(node, collection);
+=======
+  let node = collection[linkedList];
+
+  for (let i = 0; i < index; i++) {
+     node = next(headNode(linkedList, collection), collection);
+>>>>>>> a0f153bba93f9653ddfd18f558f724376f1448c1
    }
 
   return node;
 }
 
 function addressAt(index, linkedList, collection) {
+<<<<<<< HEAD
   if (index === 0) {
     return linkedList;
   } else {
     let previousNode = nodeAt(index - 1, linkedList, collection);
     return previousNode.next;
+=======
+  let head = collection[linkedList];
+  let node;
+  let i;
+
+  for (i = 0; i < index; i++) {
+     node = next(head, collection);
+   }
+
+  if (collection[linkedList] === node) {
+    return linkedList;
+>>>>>>> a0f153bba93f9653ddfd18f558f724376f1448c1
   }
 }
